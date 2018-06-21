90dd40433e4822b22f1bf914a4859e41b7fc69dd
Anthony Chen <chen.anthony.1109@gmail.com>
2018-06-21 17:49:00 +0000

    automatically backed up by learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -2,7 +2,7 @@ def first_challenge
   contacts = {
     "Jon Snow" => {
       name: "Jon",
-      email: "jon_snow@thewall.we", 
+      email: "jon_snow@thewall.we",
       favorite_icecream_flavors: ["chocolate", "vanilla", "mint chip"],
       knows: nil
     },
@@ -14,12 +14,11 @@ def first_challenge
   }
 
   #your code here
-  contacts.each.do |name|
-    name[:favorite_icecream_flavors].delete_if{|flavor| flavor == "strawberry"}
+  contacts.collect do |key, value|
+    if key == "Freddy Mercury"
+      value[:favorite_icecream_flavors].delete_if {|flavor| flavor == "strawberry"}
+    end
   end
   contacts
-
   #remember to return your newly altered contacts hash!
-  contacts
 end
-
